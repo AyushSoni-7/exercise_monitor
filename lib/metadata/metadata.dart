0b7d39fa7d11edc7400f124fe48b7ef334d47405
AyushSoni-7 <I1912@IT002272.local>
2022-03-20 15:57:39 +0530

    working navigation

--- lib/metadata/metadata.dart
+++ lib/metadata/metadata.dart
@@ -6,15 +6,23 @@ class Metadata {
     "Home",
     "Statics",
     "Calendar",
-    "Setting",
-    "Logout"
+    // "Setting",
+    // "Logout"
   ];
 
   static List<IconData> drawerMenuIcons = [
     CupertinoIcons.home,
     CupertinoIcons.graph_square,
     CupertinoIcons.calendar,
-    CupertinoIcons.settings,
-    Icons.logout
+    // CupertinoIcons.settings,
+    // Icons.logout
+  ];
+
+  static List<String> routePath = [
+    "/",
+    "/statics",
+    "/report",
+    // "/statics",
+    // "/statics"
   ];
 }
