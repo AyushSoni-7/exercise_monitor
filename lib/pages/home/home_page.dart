d8e4cbe73f711da70def414f9113e4b3ef18cb33
AyushSoni-7 <I1912@IT002272.local>
2022-03-20 13:08:28 +0530

    working graphs

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:exercise_monitor/pages/calendar_report/calendar_report.dart';
 import 'package:exercise_monitor/pages/home/drawer_page.dart';
+import 'package:exercise_monitor/pages/statics/statics.dart';
 import 'package:flutter/material.dart';
 
 import '../week_calendar/calendar.dart';
@@ -11,9 +12,10 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // return const CalendarWidget();
     return Scaffold(
-      appBar: AppBar(title: Text("Gym Monitor")),
+      appBar: AppBar(title: const Text("Gym Monitor")),
       drawer: const DrawerMenu(),
-      body: const CalendarReportWidget(),
+      // body: const CalendarReportWidget(),
+      body: const StaticsHomeWidget(),
     );
   }
 }
