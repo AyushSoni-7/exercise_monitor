e5e33f6d8848217ec9bf81fc3ac5dab3c975f1bd
AyushSoni-7 <I1912@IT002272.local>
2022-03-21 23:45:01 +0530

    changed muscle table

--- lib/models/muscle.dart
+++ lib/models/muscle.dart
@@ -1,7 +1,15 @@
+const String muscleTable = "muscle";
+
 class Muscle {
   int id;
   String name;
   String imgSrc;
 
   Muscle({required this.id, required this.name, required this.imgSrc});
+}
+
+class MuscleFields {
+  static const String id = "_id";
+  static const String name = "name";
+  static const String imgSrc = "imgSrc";
 }
